d6df42fe78a0252b278d364832b879de6fbbfd88
WorkBuddy AI <workbuddy@example.com>
2026-05-26 06:33:30 +0800

    fix package: app/src/main/java/com/osfans/trime/ime/bar/ui/CandidateUi.kt (package -> com.localtype.app)

--- app/src/main/java/com/osfans/trime/ime/bar/ui/CandidateUi.kt
+++ app/src/main/java/com/osfans/trime/ime/bar/ui/CandidateUi.kt
@@ -2,12 +2,12 @@
 //
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-package com.osfans.trime.ime.bar.ui
+package com.localtype.app.ime.bar.ui
 
 import android.content.Context
 import android.view.View
-import com.osfans.trime.R
-import com.osfans.trime.data.theme.Theme
+import com.localtype.app.R
+import com.localtype.app.data.theme.Theme
 import splitties.dimensions.dp
 import splitties.views.dsl.constraintlayout.before
 import splitties.views.dsl.constraintlayout.centerVertically
