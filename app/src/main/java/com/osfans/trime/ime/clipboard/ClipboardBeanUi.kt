dd1c7d937d245c655dc266954d92c8c4647d5a2d
WorkBuddy AI <workbuddy@example.com>
2026-05-26 06:34:33 +0800

    fix package: app/src/main/java/com/osfans/trime/ime/clipboard/ClipboardBeanUi.kt (package -> com.localtype.app)

--- app/src/main/java/com/osfans/trime/ime/clipboard/ClipboardBeanUi.kt
+++ app/src/main/java/com/osfans/trime/ime/clipboard/ClipboardBeanUi.kt
@@ -3,16 +3,16 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
-package com.osfans.trime.ime.clipboard
+package com.localtype.app.ime.clipboard
 
 import android.content.Context
 import android.text.TextUtils
 import android.view.View
-import com.osfans.trime.R
-import com.osfans.trime.data.theme.ColorManager
-import com.osfans.trime.data.theme.FontManager
-import com.osfans.trime.data.theme.Theme
-import com.osfans.trime.ime.keyboard.GestureFrame
+import com.localtype.app.R
+import com.localtype.app.data.theme.ColorManager
+import com.localtype.app.data.theme.FontManager
+import com.localtype.app.data.theme.Theme
+import com.localtype.app.ime.keyboard.GestureFrame
 import splitties.dimensions.dp
 import splitties.resources.drawable
 import splitties.views.dsl.constraintlayout.bottomOfParent
