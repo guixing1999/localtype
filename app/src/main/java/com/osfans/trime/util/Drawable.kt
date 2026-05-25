7cdd9a9d3336d0e52331e2fe84cd30c7dd0ef04d
WorkBuddy AI <workbuddy@example.com>
2026-05-26 06:39:09 +0800

    fix package: app/src/main/java/com/osfans/trime/util/Drawable.kt (package -> com.localtype.app)

--- app/src/main/java/com/osfans/trime/util/Drawable.kt
+++ app/src/main/java/com/osfans/trime/util/Drawable.kt
@@ -2,7 +2,7 @@
 //
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-package com.osfans.trime.util
+package com.localtype.app.util
 
 import android.content.res.ColorStateList
 import android.graphics.Color
