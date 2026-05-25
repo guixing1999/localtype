06cb839e35621f681e6404f9872a790136411a70
WorkBuddy AI <workbuddy@example.com>
2026-05-26 06:34:10 +0800

    fix package: app/src/main/java/com/osfans/trime/ime/candidates/popup/PopupCandidatesLayout.kt (package -> com.localtype.app)

--- app/src/main/java/com/osfans/trime/ime/candidates/popup/PopupCandidatesLayout.kt
+++ app/src/main/java/com/osfans/trime/ime/candidates/popup/PopupCandidatesLayout.kt
@@ -3,10 +3,10 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
-package com.osfans.trime.ime.candidates.popup
+package com.localtype.app.ime.candidates.popup
 
-import com.osfans.trime.R
-import com.osfans.trime.data.prefs.PreferenceDelegateEnum
+import com.localtype.app.R
+import com.localtype.app.data.prefs.PreferenceDelegateEnum
 
 enum class PopupCandidatesLayout(override val stringRes: Int) : PreferenceDelegateEnum {
     AUTOMATIC(R.string.automatic),
