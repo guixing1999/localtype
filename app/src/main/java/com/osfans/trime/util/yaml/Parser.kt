6c908bb1b3736637b175f08979a1a378323e0d97
WorkBuddy AI <workbuddy@example.com>
2026-05-26 06:40:19 +0800

    fix package: app/src/main/java/com/osfans/trime/util/yaml/Parser.kt (package -> com.localtype.app)

--- app/src/main/java/com/osfans/trime/util/yaml/Parser.kt
+++ app/src/main/java/com/osfans/trime/util/yaml/Parser.kt
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
-package com.osfans.trime.util.yaml
+package com.localtype.app.util.yaml
 
 import org.yaml.snakeyaml.LoaderOptions
 import org.yaml.snakeyaml.error.MarkedYAMLException
