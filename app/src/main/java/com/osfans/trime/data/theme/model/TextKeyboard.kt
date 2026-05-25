27851864698f195e9a376e174cb989edc0135d28
WorkBuddy AI <workbuddy@example.com>
2026-05-26 06:33:08 +0800

    fix package: app/src/main/java/com/osfans/trime/data/theme/model/TextKeyboard.kt (package -> com.localtype.app)

--- app/src/main/java/com/osfans/trime/data/theme/model/TextKeyboard.kt
+++ app/src/main/java/com/osfans/trime/data/theme/model/TextKeyboard.kt
@@ -3,18 +3,18 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
-package com.osfans.trime.data.theme.model
+package com.localtype.app.data.theme.model
 
 import android.os.Parcelable
-import com.osfans.trime.ime.keyboard.KeyBehavior
-import com.osfans.trime.util.yaml.Node
-import com.osfans.trime.util.yaml.boolean
-import com.osfans.trime.util.yaml.enum
-import com.osfans.trime.util.yaml.float
-import com.osfans.trime.util.yaml.int
-import com.osfans.trime.util.yaml.mapping
-import com.osfans.trime.util.yaml.sequence
-import com.osfans.trime.util.yaml.string
+import com.localtype.app.ime.keyboard.KeyBehavior
+import com.localtype.app.util.yaml.Node
+import com.localtype.app.util.yaml.boolean
+import com.localtype.app.util.yaml.enum
+import com.localtype.app.util.yaml.float
+import com.localtype.app.util.yaml.int
+import com.localtype.app.util.yaml.mapping
+import com.localtype.app.util.yaml.sequence
+import com.localtype.app.util.yaml.string
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
