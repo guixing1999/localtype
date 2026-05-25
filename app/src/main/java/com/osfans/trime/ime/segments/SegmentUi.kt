78dc80dfb2f8f9e2ae7312e48c585fb8d01f89ec
WorkBuddy AI <workbuddy@example.com>
2026-05-26 06:36:22 +0800

    fix package: app/src/main/java/com/osfans/trime/ime/segments/SegmentUi.kt (package -> com.localtype.app)

--- app/src/main/java/com/osfans/trime/ime/segments/SegmentUi.kt
+++ app/src/main/java/com/osfans/trime/ime/segments/SegmentUi.kt
@@ -3,17 +3,17 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
-package com.osfans.trime.ime.segments
+package com.localtype.app.ime.segments
 
 import android.content.Context
 import android.content.res.ColorStateList
 import android.graphics.drawable.StateListDrawable
 import android.view.ViewOutlineProvider
 import com.google.android.flexbox.FlexboxLayoutManager
-import com.osfans.trime.data.theme.ColorManager
-import com.osfans.trime.data.theme.FontManager
-import com.osfans.trime.data.theme.Theme
-import com.osfans.trime.ime.keyboard.GestureFrame
+import com.localtype.app.data.theme.ColorManager
+import com.localtype.app.data.theme.FontManager
+import com.localtype.app.data.theme.Theme
+import com.localtype.app.ime.keyboard.GestureFrame
 import splitties.dimensions.dp
 import splitties.views.dsl.core.Ui
 import splitties.views.dsl.core.add
