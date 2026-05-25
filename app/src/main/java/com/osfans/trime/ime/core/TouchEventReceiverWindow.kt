b545af42ebb357f89520dbe363a1e58db00706ff
WorkBuddy AI <workbuddy@example.com>
2026-05-26 06:35:20 +0800

    fix package: app/src/main/java/com/osfans/trime/ime/core/TouchEventReceiverWindow.kt (package -> com.localtype.app)

--- app/src/main/java/com/osfans/trime/ime/core/TouchEventReceiverWindow.kt
+++ app/src/main/java/com/osfans/trime/ime/core/TouchEventReceiverWindow.kt
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
-package com.osfans.trime.ime.core
+package com.localtype.app.ime.core
 
 import android.annotation.SuppressLint
 import android.view.Gravity
