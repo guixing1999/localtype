e2b2a2ac5474eff5ca42b1ead5ff5331ad130dbf
WorkBuddy AI <workbuddy@example.com>
2026-05-26 06:39:16 +0800

    fix package: app/src/main/java/com/osfans/trime/util/Exception.kt (package -> com.localtype.app)

--- app/src/main/java/com/osfans/trime/util/Exception.kt
+++ app/src/main/java/com/osfans/trime/util/Exception.kt
@@ -5,7 +5,7 @@
 /**
  * Adapted from [fcitx5-android/Exception](https://github.com/fcitx5-android/fcitx5-android/blob/e44c1c7/app/src/main/java/org/fcitx/fcitx5/android/utils/Exception.kt)
  */
-package com.osfans.trime.util
+package com.localtype.app.util
 
 import androidx.annotation.StringRes
 
